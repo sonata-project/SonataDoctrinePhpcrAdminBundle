6b0af07082aa264a4d9631cf4c46aca86b2b1b55
Nacho Martín <nitram.ohcan@gmail.com>
2011-11-04 00:56:03 +0100

    #5 phpdoc for Datagrid/ProxyQuery

--- Datagrid/ProxyQuery.php
+++ Datagrid/ProxyQuery.php
@@ -17,28 +17,71 @@ use PHPCR\Query\QOM\QueryObjectModelConstantsInterface as Constants;
 use PHPCR\Util\QOM\QueryBuilder;
 
 /**
- * This class try to unify the query usage with Doctrine
+ * This class is used to abstract the Admin Bundle from the different QueryBuilder implementations
  */
 class ProxyQuery implements ProxyQueryInterface
 {
+    /**
+     * QOM factory used to get access to the QueryBuilder and its parameters
+     *
+     * @var \PHPCR\Query\QOM\QueryObjectModelFactoryInterface
+     */
     protected $qomFactory;
 
+    /**
+     * Query Builder Fluent interface for the QOM
+     *
+     * @var \PHPCR\Util\QOM\QueryBuilder
+     */
     protected $qb;
 
+    /**
+     * Property that determines the Ordering of the results
+     *
+     * @var string
+     */
     protected $sortBy;
 
+    /**
+     * Ordering of the results (ASC, DESC)
+     *
+     * @var string
+     */
     protected $sortOrder;
 
+    /**
+     * PHPCR ODM Document Manager
+     *
+     * @var \Doctrine\ODM\PHPCR\DocumentManager;
+     */
     protected $documentManager;
 
+    /**
+     * Name of this document class
+     *
+     * @var string
+     */
     protected $documentName;
 
+    /**
+     * Creates a Query Builder from the QOMFactory
+     *
+     * @param \PHPCR\Query\QOM\QueryObjectModelFactoryInterface $qomFactory
+     */
     public function __construct(QueryObjectModelFactoryInterface $qomFactory)
     {
         $this->qomFactory = $qomFactory;
         $this->qb = new QueryBuilder($qomFactory);
     }
 
+    /**
+     * Executes the query, applying the source, the constraint of documents being of the phpcr:class of
+     * this kind of document and builds an array of retrieved documents.
+     *
+     * @param array $params doesn't have any effect
+     * @param mixed $hydrationMode doesn't have any effect
+     * @return aray of documents
+     */
     public function execute(array $params = array(), $hydrationMode = null)
     {
         $qf = $this->qomFactory;
@@ -65,26 +108,53 @@ class ProxyQuery implements ProxyQueryInterface
 
     }
 
+    /**
+     * Allows for direct calls to the QueryBuilder. TODO: I am not sure it this should exist in PHPCR context.
+     *
+     * @param string $name name of the method
+     * @param array $args arguments of the call
+     * @return void
+     */
     public function __call($name, $args)
     {
         return call_user_func_array(array($this->qb, $name), $args);
     }
 
+    /**
+     * Set the property to be sorted by
+     *
+     * @param string $sortBy property name
+     */
     public function setSortBy($sortBy)
     {
         $this->sortBy = $sortBy;
     }
 
+    /**
+     * Gets the property that defines the ordering
+     *
+     * @return string the property to be sorted by
+     */
     public function getSortBy()
     {
         return $this->sortBy;
     }
 
+    /**
+     * Sets the ordering
+     *
+     * @param string $sortOrder (ASC|DESC)
+     */
     public function setSortOrder($sortOrder)
     {
         $this->sortOrder = $sortOrder;
     }
 
+    /**
+     * Gets the ordering
+     *
+     * @return string (ASC|DESC)
+     */
     public function getSortOrder()
     {
         return $this->sortOrder;
@@ -92,40 +162,75 @@ class ProxyQuery implements ProxyQueryInterface
 
     public function getSingleScalarResult()
     {
-        /* FIX: What to do here? */
+        /* TODO: Figure out who calls this method and what to do here in context of PHPCR */
     }
 
 
+    /**
+     * Gets the QueryBuilder
+     *
+     * @return \PHPCR\Util\QOM\QueryBuilder
+     */
     public function getQueryBuilder()
     {
-      return $this->qomFactory;
+      return $this->qb;
     }
 
+    /**
+     * Sets the first result (offset)
+     *
+     * @param integer $firstResult
+     */
     public function setFirstResult($firstResult)
     {
         $this->qb->setFirstResult($firstResult);
     }
 
+    /**
+     * Gets the first result (offset)
+     *
+     * @return integer the offset
+     */
     public function getFirstResult()
     {
         return $this->qb->getFirstResult();
     }
 
+    /**
+     * Set maximum number of results to retrieve
+     *
+     * @param integer $maxResults
+     */
     public function setMaxResults($maxResults)
     {
         $this->qb->setMaxResults($maxResults);
     }
 
+    /**
+     * Gets the maximum number of results to retrieve
+     *
+     * @return integer
+     */
     public function getMaxResults()
     {
         return $this->qb->getMaxResults();
     }
 
+    /**
+     * Sets the document manager
+     *
+     * @param \Doctrine\ODM\PHPCR\DocumentManager $documentManager
+     */
     public function setDocumentManager(DocumentManager $documentManager)
     {
         $this->documentManager = $documentManager;
     }
 
+    /**
+     * Sets the document name (Class of the document)
+     *
+     * @param string $documentName
+     */
     public function setDocumentName($documentName)
     {
         $this->documentName = $documentName;
