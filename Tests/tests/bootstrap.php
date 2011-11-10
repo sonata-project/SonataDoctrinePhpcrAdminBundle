494dd8ef29350afbc6cf587ef8f71772603e8bfc
Nacho Martín <nitram.ohcan@gmail.com>
2011-11-10 01:34:25 +0100

    Testing placeholder

--- Tests/tests/bootstrap.php
+++ Tests/tests/bootstrap.php
@@ -9,17 +9,7 @@
  * file that was distributed with this source code.
  */
 
-$rootDir = __DIR__.'/../../../../../';
+$rootDir = __DIR__.'/../../../../../../';
 
 require_once $rootDir.'/vendor/symfony/src/Symfony/Component/ClassLoader/UniversalClassLoader.php';
 require_once $rootDir.'/app/autoload.php';
-
-if (!class_exists('PHPUnit_Framework_TestCase') ||
-    version_compare(PHPUnit_Runner_Version::id(), '3.5') < 0
-) {
-    die('PHPUnit framework is required, at least 3.5 version');
-}
-
-if (!class_exists('PHPUnit_Framework_MockObject_MockBuilder')) {
-    die('PHPUnit MockObject plugin is required, at least 1.0.8 version');
-}
