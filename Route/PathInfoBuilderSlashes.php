9a651a3c0c738306ddbe0d6e5c710bde9e7be16e
Nacho Martín <nitram.ohcan@gmail.com>
2012-01-24 23:46:16 +0100

    Tree view

--- Route/PathInfoBuilderSlashes.php
+++ Route/PathInfoBuilderSlashes.php
@@ -28,7 +28,7 @@ class PathInfoBuilderSlashes implements RouteBuilderInterface
         $collection->add('list');
         $collection->add('create');
         $collection->add('batch');
-        $collection->add('edit', $admin->getRouterIdParameter().'/edit', array(), array('id' => '.+','_method' => 'GET' ));
+        $collection->add('edit', $admin->getRouterIdParameter().'/edit', array(), array('id' => '.+'));
         $collection->add('delete', $admin->getRouterIdParameter().'/delete', array(), array('id' => '.+'));
         $collection->add('show', $admin->getRouterIdParameter(), array(), array('id' => '.+', '_method' => 'GET'));
 
