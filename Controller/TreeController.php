60658231c2c24304a04643e69eb172f59809c81c
Jacopo Jakuza Romei <jromei@gmail.com>
2012-02-10 16:07:16 +0100

    Keeping tree state with stateless strategy :-)

--- Controller/TreeController.php
+++ Controller/TreeController.php
@@ -15,31 +15,31 @@ class TreeController extends Controller
      *
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function indexAction() {
+    public function treeAction($id) {
+        
         //Obtain the routes for each document
         $pool = $this->get('sonata.admin.pool');
         $classes = $pool->getAdminClasses();
         $adminClasses = array();
+        
         foreach ($classes as $class) {
             $instance = $this->get($class);
             $routeCollection = array();
             foreach ($instance->getRoutes()->getElements() as $code => $route) {
                 $action = explode('.', $code);
-                //the key is "create", "delete"... and the value is the name
-                //of the route
                 $routeCollection[end($action)] = sprintf('%s_%s', $instance->getBaseRouteName(), end($action));
             }
             array_push($adminClasses, array(
-                'label' => $instance->getLabel(),
+                'label'     => $instance->getLabel(),
                 'className' => $instance->getClass(),
                 'baseRoute' => $instance->getBaseRoutePattern(),
-                'routes' => $routeCollection));
+                'routes'    => $routeCollection));
         }
 
-        return $this->render('SonataDoctrinePHPCRAdminBundle:Tree:index.html.twig', array(
-            'base_template'   => 'SonataAdminBundle::standard_layout.html.twig',
-            'admin_pool'      => $this->container->get('sonata.admin.pool'),
-            'handlers'          => $adminClasses
+        return $this->render('SonataDoctrinePHPCRAdminBundle:Tree:tree.html.twig', array(
+            'id'            => $id,
+            'admin_pool'    => $this->container->get('sonata.admin.pool'),
+            'handlers'      => $adminClasses
         ));
     }
 }
