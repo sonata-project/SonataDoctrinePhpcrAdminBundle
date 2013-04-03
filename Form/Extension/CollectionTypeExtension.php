bb3ac31916f6b9d9df64084a307eb7ba3e5f4999
David Buchmann <david.buchmann@liip.ch>
2013-04-03 22:54:30 +0200

    adding a form type extension to make sonata-type-collection reorder automatically so reorderings need no further handling

---
+++ Form/Extension/CollectionTypeExtension.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+ * This file is part of the Sonata package.
+ *
+ * (c) Thomas Rabaix <thomas.rabaix@sonata-project.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sonata\DoctrinePHPCRAdminBundle\Form\Extension;
+
+use Doctrine\Common\Util\Debug;
+use Symfony\Component\Form\AbstractTypeExtension;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvents;
+
+use Sonata\DoctrinePHPCRAdminBundle\Form\Listener\CollectionOrderListener;
+
+/**
+ * Extend the sonata collection type to sort the collection so the reordering
+ * is automatically persisted in phpcr-odm.
+ */
+class CollectionTypeExtension extends AbstractTypeExtension
+{
+
+    /**
+     * {@inheritDoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $listener = new CollectionOrderListener($options['sonata_field_description']->getName());
+        $builder->getParent()->addEventListener(FormEvents::BIND, array($listener, 'onPostBind'));
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getExtendedType()
+    {
+        return 'sonata_type_collection';
+    }
+}
