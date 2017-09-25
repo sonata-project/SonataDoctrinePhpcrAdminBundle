a1b45dc202bae634a8fb1d6786bf69d0dd35f6fd
Sonata CI <thomas+ci@sonata-project.org>
2017-09-25 07:51:19 +0200

    DevKit updates (#496)

--- .travis/before_install_test.sh
+++ .travis/before_install_test.sh
@@ -6,15 +6,11 @@ if [ "${TRAVIS_PHP_VERSION}" != "hhvm" ]; then
     TRAVIS_INI_FILE="$PHP_INI_DIR/travis.ini"
     echo "memory_limit=3072M" >> "$TRAVIS_INI_FILE"
 
-        if [ "$TRAVIS_PHP_VERSION" '<' '5.4' ]; then
-        XDEBUG_INI_FILE="$PHP_INI_DIR/xdebug.ini"
-        if [ -f  "$XDEBUG_INI_FILE" ]; then
-            mv "$XDEBUG_INI_FILE" /tmp
-        fi
-    fi
     
     fi
 
 sed --in-place "s/\"dev-master\":/\"dev-${TRAVIS_COMMIT}\":/" composer.json
 
 if [ "$SYMFONY" != "" ]; then composer require "symfony/symfony:$SYMFONY" --no-update; fi;
+if [ "$SONATA_ADMIN" != "" ]; then composer require "sonata-project/admin-bundle:$SONATA_ADMIN" --no-update; fi;
+if [ "$SONATA_BLOCK" != "" ]; then composer require "sonata-project/block-bundle:$SONATA_BLOCK" --no-update; fi;
