387bf19248bfffde72dfcc444fc47099c525eea1
Sullivan SENECHAL <soullivaneuh@gmail.com>
2019-01-13 11:02:32 +0100

    Applied fixes from FlintCI

--- src/Form/Type/Filter/ChoiceType.php
+++ src/Form/Type/Filter/ChoiceType.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 /*
  * This file is part of the Sonata Project package.
  *
@@ -17,7 +19,7 @@ use Symfony\Component\Form\FormBuilderInterface;
 
 class ChoiceType extends BaseChoiceType
 {
-    const TYPE_CONTAINS_WORDS = 4;
+    public const TYPE_CONTAINS_WORDS = 4;
 
     /**
      * {@inheritdoc}
