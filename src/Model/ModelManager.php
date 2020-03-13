0a21c5ebb5ac89a6d3a01f9d1e53dede7a101595
Vincent Langlet <misterdeviling@gmail.com>
2020-03-13 14:50:36 +0100

    Use private method

--- src/Model/ModelManager.php
+++ src/Model/ModelManager.php
@@ -374,7 +374,7 @@ class ModelManager implements ModelManagerInterface
     {
         $values = $datagrid->getValues();
 
-        if (isset($values['_sort_by']) && $fieldDescription->getName() === $values['_sort_by']->getName()) {
+        if ($this->isFieldAlreadySorted($fieldDescription, $datagrid)) {
             if ('ASC' === $values['_sort_order']) {
                 $values['_sort_order'] = 'DESC';
             } else {
@@ -397,7 +397,7 @@ class ModelManager implements ModelManagerInterface
     {
         $values = $datagrid->getValues();
 
-        if (isset($values['_sort_by'])) {
+        if (isset($values['_sort_by']) && $values['_sort_by'] instanceof FieldDescriptionInterface) {
             $values['_sort_by'] = $values['_sort_by']->getName();
         }
         $values['_page'] = $page;
@@ -551,5 +551,17 @@ class ModelManager implements ModelManagerInterface
     protected function camelize($property)
     {
         return preg_replace(['/(^|_)+(.)/e', '/\.(.)/e'], ["strtoupper('\\2')", "'_'.strtoupper('\\1')"], $property);
+    }
+
+    private function isFieldAlreadySorted(FieldDescriptionInterface $fieldDescription, DatagridInterface $datagrid): bool
+    {
+        $values = $datagrid->getValues();
+
+        if (!isset($values['_sort_by']) || !$values['_sort_by'] instanceof FieldDescriptionInterface) {
+            return false;
+        }
+
+        return $values['_sort_by']->getName() === $fieldDescription->getName()
+            || $values['_sort_by']->getName() === $fieldDescription->getOption('sortable');
     }
 }
