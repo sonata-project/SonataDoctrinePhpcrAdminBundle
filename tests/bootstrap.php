b8865d034a1cde8f1810c4de4463cb078f3ac116
Vincent Langlet <vincentlanglet@hotmail.fr>
2021-03-16 20:27:29 +0100

    Update test config

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -11,50 +11,13 @@ declare(strict_types=1);
  * file that was distributed with this source code.
  */
 
-use Sonata\DoctrinePHPCRAdminBundle\Tests\Fixtures\App\Kernel;
-use Symfony\Bundle\FrameworkBundle\Console\Application;
-use Symfony\Component\Console\Input\ArrayInput;
-use Symfony\Component\Console\Output\ConsoleOutput;
-
 /*
  * fix encoding issue while running text on different host with different locale configuration
  */
 setlocale(\LC_ALL, 'en_US.UTF-8');
 
-if (file_exists($file = __DIR__.'/autoload.php')) {
-    require_once $file;
-} elseif (file_exists($file = __DIR__.'/autoload.php.dist')) {
+require_once __DIR__.'/../vendor/autoload.php';
+
+if (file_exists($file = __DIR__.'/custom_bootstrap.php')) {
     require_once $file;
 }
-
-/*
- * try to get Symfony's PHPUnit Bridge
- */
-$files = array_filter([
-    __DIR__.'/../vendor/symfony/symfony/src/Symfony/Bridge/PhpUnit/bootstrap.php',
-    __DIR__.'/../vendor/symfony/phpunit-bridge/bootstrap.php',
-    __DIR__.'/../../../../vendor/symfony/symfony/src/Symfony/Bridge/PhpUnit/bootstrap.php',
-    __DIR__.'/../../../../vendor/symfony/phpunit-bridge/bootstrap.php',
-], 'file_exists');
-
-if ($files) {
-    require_once current($files);
-}
-
-require_once __DIR__.'/../vendor/symfony-cmf/testing/bootstrap/bootstrap.php';
-
-$application = new Application(new Kernel());
-$application->setAutoExit(false);
-
-// Load fixtures of the AppTestBundle
-$input = new ArrayInput([
-    'command' => 'doctrine:phpcr:init:dbal',
-    '--drop' => true,
-    '--force' => true,
-]);
-$application->run($input, new ConsoleOutput());
-
-$input = new ArrayInput([
-    'command' => 'doctrine:phpcr:repository:init',
-]);
-$application->run($input, new ConsoleOutput());
